2c5d7001b10e3c439cb9673e9e06e13ecba12a85
alexander nunez <alexandernunz@outlook.com>
2023-02-16 15:27:42 -0500

    rough draft

---
+++ Develop/models/Category.js
@@ -0,0 +1,21 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    // define columns
+    id: DataTypes
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
